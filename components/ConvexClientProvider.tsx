6f19c1272608e877a1f4f022e5f0dfa97a7f4388
Atharva Raut <atharvaraut7126@gmail.com>
2026-09-13 20:37:57 +0530

    fix: error in ConvexClientProvider

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -4,7 +4,7 @@ import { ConvexBetterAuthProvider } from "@convex-dev/better-auth/react";
 import { ConvexReactClient } from "convex/react";
 import { authClient } from "@/lib/auth-client";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL! || process.env.CONVEX_URL!);
 
 export function ConvexClientProvider({
   children,
